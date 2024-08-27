7166b7b0d5f26a6a7e35435c2dd28f80cc5f73f2
eduard <eduard.vish@rambler.ru>
2024-08-27 21:38:18 +0300

    Add file

---
+++ main.py
@@ -0,0 +1,9 @@
+# Task from 27/08/2024.
+
+example = 'Бругмансия' #1
+print(example[0])      #2
+print(example[-1])     #3
+print(example[5:])     #4
+print(example[::-1])   #5
+print(example[1:10:2])   #6
+
