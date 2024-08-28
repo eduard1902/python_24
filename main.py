fd4716e320a769e9e28d1f46fbc0520c6cf45eb9
Эдуард <54848296+eduard1902@users.noreply.github.com>
2024-08-28 09:34:55 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,9 @@
+# Practical assignment on the topic "Variables"
+
+number_of_tasks = 12
+number_of_hours = 1.5
+course_name = 'Python'
+time_of_one_task = (number_of_hours/number_of_tasks)
+a = ","
+b =':'
+print('Курс:',str(course_name) + a ,'всего задач' + b + str(number_of_tasks) +a,'затрачено часов: ', str(number_of_hours) + a, 'среднее время выполнения ', time_of_one_task, ' часа.')
