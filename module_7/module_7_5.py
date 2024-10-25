1f253f46277ed785736a2e4ada9d50fd133c02dd
Эдуард <54848296+eduard1902@users.noreply.github.com>
2024-10-25 15:43:23 +0300

    Add files via upload

---
+++ module_7/module_7_5.py
@@ -0,0 +1,45 @@
+import os
+import time
+
+directory = '.'
+
+for root, dirs, files in os.walk(directory):
+    for file in files:
+        filepath = os.path.join(root, file)
+        filetime = os.path.getmtime(filepath)
+        formatted_time = time.strftime("%d.%m.%Y %H:%M", time.localtime())
+        filesize = os.path.getsize(filepath)
+        parent_dir = os.path.abspath(directory)
+        # parent_dir = os.path.dirname(os.getcwd())
+        print(f'Обнаружен файл: {file}, Путь: {filepath}, Размер: {filesize} байт, Время изменения: {formatted_time}, Родительская директория: {parent_dir}')
+
+
+
+    # for file in files:
+    #         if file.endswith('.txt'):
+    #             print(os.path.join(root, file))
+
+
+
+"""
+Создайте новый проект или продолжите работу в текущем проекте.
+Используйте os.walk для обхода каталога, путь к которому указывает переменная directory
+Примените os.path.join для формирования полного пути к файлам.
+Используйте os.path.getmtime и модуль time для получения и отображения времени последнего изменения файла.
+Используйте os.path.getsize для получения размера файла.
+Используйте os.path.dirname для получения родительской директории файла.
+
+Комментарии к заданию:
+
+Ключевая идея – использование вложенного for
+
+for root, dirs, files in os.walk(directory):
+  for file in files:
+    filepath = ?
+    filetime = ?
+    formatted_time = time.strftime("%d.%m.%Y %H:%M", time.localtime(filetime))
+    filesize = ?
+    parent_dir = ?
+    print(f'Обнаружен файл: {file}, Путь: {filepath}, Размер: {filesize} байт, Время изменения: {formatted_time}, Родительская директория: {parent_dir}')
+
+"""
