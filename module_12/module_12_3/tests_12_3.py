6aae58ce51c92e4dcb547816c5a97de5f7496aa9
Эдуард <54848296+eduard1902@users.noreply.github.com>
2024-12-16 21:25:53 +0300

    Add files via upload
    "Систематизация и пропуск тестов".
    Цель: понять на практике как объединять тесты при помощи TestSuite. Научиться пропускать тесты при помощи встроенных в unittest декораторов.
    
    Задача "Заморозка кейсов":
    Подготовка:
    В этом задании используйте те же TestCase, что и в предыдущем: RunnerTest и TournamentTest.
    Часть 1. TestSuit.
    Создайте модуль suite_12_3.py для описания объекта TestSuite. Укажите на него переменной с произвольным названием.
    Добавьте тесты RunnerTest и TournamentTest в этот TestSuit.
    Создайте объект класса TextTestRunner, с аргументом verbosity=2.
    Часть 2. Пропуск тестов.
    Классы RunnerTest дополнить атрибутом is_frozen = False и TournamentTest атрибутом is_frozen = True.
    Напишите соответствующий декоратор к каждому методу (кроме @classmethod), который при значении is_frozen = False будет выполнять тесты, а is_frozen = True - пропускать и выводить сообщение 'Тесты в этом кейсе заморожены'.
    Таким образом вы сможете контролировать пропуск всех тестов в TestCase изменением всего одного атрибута.
    Запустите TestSuite и проверьте полученные результаты тестов из обоих TestCase.
    Пример результата выполнения тестов:
    Вывод на консоль:
    test_challenge (tests_12_3.RunnerTest.test_challenge) ... ok
    test_run (tests_12_3.RunnerTest.test_run) ... ok
    test_walk (tests_12_3.RunnerTest.test_walk) ... ok
    test_first_tournament (tests_12_3.TournamentTest.test_first_tournament) ... skipped 'Тесты в этом кейсе заморожены'
    test_second_tournament (tests_12_3.TournamentTest.test_second_tournament) ... skipped 'Тесты в этом кейсе заморожены'
    test_third_tournament (tests_12_3.TournamentTest.test_third_tournament) ... skipped 'Тесты в этом кейсе заморожены'
    ----------------------------------------------------------------------
    Ran 6 tests in 0.000s OK (skipped=3)

---
+++ module_12/module_12_3/tests_12_3.py
@@ -0,0 +1,69 @@
+from runner_and_tournament import Runner, Tournament
+import unittest
+
+class RunnerTest(unittest.TestCase):
+
+    is_frozen = False
+
+    def test_walk(self):
+        first = Runner('a')
+        for i in range(10):
+            first.walk()
+        self.assertEqual(first.distance,  50)
+
+    def test_run(self):
+        second = Runner('b')
+        for i in range(10):
+            second.run()
+        self.assertEqual(second.distance,  100)
+
+    def test_challenge(self):
+        first = Runner('a')
+        second = Runner('b')
+        for i in range(10):
+            first .walk()
+            second.run()
+        self.assertNotEqual(first.distance, second.distance)
+
+class TournamentTest(unittest.TestCase):
+
+    s_frozen = True
+
+    @classmethod
+    def setUpClass(cls):
+        cls.all_results = []
+
+    def setUp(self):
+        self.usain = Runner("Усэйн", 10)
+        self.andrey = Runner("Андрей", 9)
+        self.nik = Runner("Ник", 3)
+
+    @classmethod
+    def tearDownClass(cls):
+        for result in cls.all_results:
+            print(result)
+
+    @unittest.skipIf(s_frozen, 'Тесты в этом кейсе заморожены')
+    def test_first_tournament (self):
+        race = Tournament(90, self.usain, self.nik)
+        resault = {key: str(value) for key, value in race.start().items()}
+        TournamentTest.all_results.append(resault)
+        self.assertEqual(resault[2],"Ник")
+
+    @unittest.skipIf(s_frozen, 'Тесты в этом кейсе заморожены')
+    def test_second_tournament(self):
+        race = Tournament(90, self.andrey, self.nik)
+        resault = {key: str(value) for key, value in race.start().items()}
+        TournamentTest.all_results.append(resault)
+        self.assertEqual(resault[2],"Ник")
+
+    @unittest.skipIf(s_frozen, 'Тесты в этом кейсе заморожены')
+    def test_third_tournament(self):
+        race = Tournament(90, self.usain, self.andrey, self.nik)
+        resault = {key: str(value) for key, value in race.start().items()}
+        TournamentTest.all_results.append(resault)
+        self.assertEqual(resault[3],"Ник")
+
+
+if __name__ == "__main__":
+    unittest.main()
