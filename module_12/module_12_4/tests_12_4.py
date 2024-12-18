55e6c2a4faa0691be19f02d102c449b67634f175
Эдуард <54848296+eduard1902@users.noreply.github.com>
2024-12-18 21:23:47 +0300

    Add files via upload

--- module_12/module_12_4/tests_12_4.py
+++ module_12/module_12_4/tests_12_4.py
@@ -1,24 +1,33 @@
-Задача "Логирование бегунов":
-В первую очередь скачайте исходный код, который нужно обложить тестами с GitHub. (Можно скопировать)
-Основное обновление - выбрасывание исключений, если передан неверный тип в name и если передано отрицательное значение в speed.
+import logging
+import unittest
+from rt_with_exceptions import Runner
 
-Для решения этой задачи вам понадобиться класс RunnerTest из предыдущей задачи.
-В модуле tests_12_4.py импортируйте пакет logging и настройте basicConfig на следующие параметры:
-Уровень - INFO
-Режим - запись с заменой('w')
-Название файла - runner_tests.log
-Кодировка - UTF-8
-Формат вывода - на своё усмотрение, обязательная информация: уровень логирования, сообщение логирования.
+logging.basicConfig(level=logging.INFO, filemode="w", filename="runner_tests.log", encoding="UTF-8", format="%(asctime)s | %(levelname)s | %(message)s")
 
-Дополните методы тестирования в классе RunnerTest следующим образом:
-test_walk:
-Оберните основной код конструкцией try-except.
-При создании объекта Runner передавайте отрицательное значение в speed.
-В блок try добавьте логирование INFO с сообщением '"test_walk" выполнен успешно'
-В блоке except обработайте исключение соответствующего типа и логируйте его на уровне WARNING с сообщением "Неверная скорость для Runner".
-test_run:
-Оберните основной код конструкцией try-except.
-При создании объекта Runner передавайте что-то кроме строки в name.
-В блок try добавьте логирование INFO с сообщением '"test_run" выполнен успешно'
-В блоке except обработайте исключение соответствующего типа и логируйте его на уровне WARNING с сообщением "Неверный тип данных для объекта Runner".
+class RunnerTest(unittest.TestCase):
 
+
+
+    def test_walk(self):
+        try:
+            first = Runner("Клод", -5) # speed: 5 (for выполнен успешно)
+            for i in range(10):
+                first.walk()
+            self.assertEqual(first.distance, 50, "test_walk выполнен успешно")
+            # logging.info('"test_walk" выполнен успешно')
+        except  ValueError as e:
+            logging.warning('Неверная скорость для Runner', exc_info=True)
+
+
+    def test_run(self):
+        try:
+            second = Runner(int)
+            for i in range(10):
+                second.run()
+            self.assertEqual(second.distance, 100, "test_run выполнен успешно")
+        except TypeError as e :
+            logging.warning('Неверный тип данных для объекта Runner', exc_info=True)
+
+
+if __name__ == "__main__":
+    unittest.main()
